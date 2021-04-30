cc00359f4bb5cab9e5e6c6e90ff9d2ac5e22de91
rachad <rachadaccoumeh@gmail.com>
2021-04-30 18:09:44 +0300

    fix some problem

--- app/src/main/java/com/rachad/alarmmangerservice/MainActivity.java
+++ app/src/main/java/com/rachad/alarmmangerservice/MainActivity.java
@@ -1,12 +1,10 @@
 package com.rachad.alarmmangerservice;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
+
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.SystemClock;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
